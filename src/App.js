b5ab1c8aa6ca01659432cb94323d0a558e2f0b6f
Teresa Tetens <teresa.tetens@gmail.com>
2020-12-20 17:21:11 +0100

    resume with react-pdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import testPdf from "./test.pdf";
+import "./styles.css";
 
-function App() {
+// More examples here
+// https://github.com/wojtekmaj/react-pdf/wiki/Recipes
+
+// You could also pass the file as a URL to the Document component as such:
+const url =
+  "https://s3-ap-southeast-1.amazonaws.com/happay-local/HVP/BILL20198261213473719445688HP.pdf";
+
+const App = () => {
+  // PDFjs worker from an external cdn
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setPageNumber(1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Document file={testPdf} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
